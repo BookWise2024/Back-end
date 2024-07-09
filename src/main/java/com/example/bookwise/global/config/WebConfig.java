6741c5144acf7b62cbcd2b2dfa01c94442f224bd
donghui <louisp0215@gmail.com>
2024-07-10 02:30:36 +0900

    hotfix : cors 오류수정#42

--- src/main/java/com/example/bookwise/global/config/WebConfig.java
+++ src/main/java/com/example/bookwise/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","http://localhost:5000","http://13.125.215.49:5000","http://15.164.12.78:5173")    // 도메인 나오면 추가
+                .allowedOrigins("http://localhost:5173","http://localhost:5000","http://13.125.215.49:5000","http://15.164.12.78:4173")    // 도메인 나오면 추가
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
